4f1160b647ae3fb42a2d9486bbe94b126d80946a
Anup <106077220+anup2903@users.noreply.github.com>
2024-09-12 11:41:25 +0530

    main page updated

--- my-next-app/src/pages/index.tsx
+++ my-next-app/src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 
 import React,{ useEffect } from "react";
-
+import Head from "next/head";
 export default function Home() {
   const redirectButton = ()=>{
     window.location.href=`https://whereuelevate.com`;
@@ -14,10 +14,41 @@ export default function Home() {
     }, 10);
   }, []);
   return (
-    <div style={{display:"flex" ,flexDirection:"column",justifyContent:"center" ,margin:"0 auto" ,alignItems:"center",backgroundClip:"white" ,height:"100vh" }}>
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="Where U Elevate: Connecting next-gen innovative workforce with industry"
+          data-react-helmet="true"
+        />
+        <meta property="og:description" content="Find the best suited opportunity to showcase your innovative skills with available jobs, hackathons, industry led open innovation programs and career mentoring under saarthi initiative" data-react-helmet="true"/>
+        <meta property="og:image" content="../mainLogo.png"/>
+      </Head>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          margin: "0 auto",
+          alignItems: "center",
+          backgroundClip: "white",
+          height: "100vh",
+        }}
+      >
         <h1>Redirecting to https://whereuelevate.com</h1>
         <h1>Click here to redirect :</h1>
-        <button onClick={redirectButton} style={{padding:"10px" , borderRadius:"30px" , backgroundColor:"grey",color:"white"}}>Redirect</button>
+        <button
+          onClick={redirectButton}
+          style={{
+            padding: "10px",
+            borderRadius: "30px",
+            backgroundColor: "grey",
+            color: "white",
+          }}
+        >
+          Redirect
+        </button>
       </div>
+    </>
   );
 }
